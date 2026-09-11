c9958c5260e3e80e0ebfbc300707715298c6e383
harshinibabu1999-cmd <harshinibabu1999@gmail.com>
2026-09-11 11:49:45 +0530

    Fix default media storage

--- myprjt2/settings.py
+++ myprjt2/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
